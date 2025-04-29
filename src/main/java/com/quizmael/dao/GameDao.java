509a1445642ab22823e3d87e2c28b5fdcd8970de
Ismael-Reina <ismaelreina.tech@gmail.com>
2025-04-29 23:54:03 +0200

    feat: Add JavaDoc to GameDao class

--- src/main/java/com/quizmael/dao/GameDao.java
+++ src/main/java/com/quizmael/dao/GameDao.java
@@ -14,13 +14,39 @@ import java.util.Optional;
  */
 public interface GameDao {
 
+    /**
+     * Saves a new game to the database.
+     *
+     * @param game The {@link Game} entity to be saved.
+     */
     void save(Game game);
 
+    /**
+     * Updates an existing game in the database.
+     *
+     * @param game The {@link Game} entity to be updated.
+     */
     void update(Game game);
 
+    /**
+     * Deletes a game from the database.
+     *
+     * @param game The {@link Game} entity to be deleted.
+     */
     void delete(Game game);
 
+    /**
+     * Finds a game by its ID.
+     *
+     * @param id The ID of the game.
+     * @return An {@link Optional} containing the found game, or empty if not found.
+     */
     Optional<Game> findById(Integer id);
 
+    /**
+     * Retrieves all games from the database.
+     *
+     * @return A list of all {@link Game} entities.
+     */
     List<Game> findAll();
 }
